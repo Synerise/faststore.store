9d68000d14dd7738762e6676d0a751fb2ca8249a
kamilkanik <kamil.kanik@icloud.com>
2025-04-04 15:23:10 +0200

    Build fix

--- src/scripts/ThirdPartyScripts.tsx
+++ src/scripts/ThirdPartyScripts.tsx
@@ -1,31 +1,30 @@
+
 const ThirdPartyScripts = () => {
     return (
-        <>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `
-                          function onSyneriseLoad() {
-                              SR.init({
-                                  trackerKey: "7d97fd94-647a-489c-8be6-4d40b4c0eca2",
-                                  plugin: "faststore",
-                              }).then(function () {
-                                SR.event.pageVisit().then(function () {
-                                  SR.dynamicContent.get();
-                                });
-                              });
-                          }
-                        
-                          (function(s,y,n,e,r,i,se){s["SyneriseObjectNamespace"]=r;s[r]=s[r]||[],
-                           s[r]._t=1*new Date(),s[r]._i=0,s[r]._l=i;var z=y.createElement(n),
-                           se=y.getElementsByTagName(n)[0];z.async=1;z.src=e;se.parentNode.insertBefore(z,se);
-                           z.onload=z.onreadystatechange=function(){var rdy=z.readyState;
-                           if(!rdy||/complete|loaded/.test(z.readyState)){s[i]();z.onload = null;
-                           z.onreadystatechange=null;}};})(window,document,"script", "//snrcsi.blob.core.windows.net/js-sdk/synerise-javascript-sdk.js", "SR", "onSyneriseLoad");
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
+                    function onSyneriseLoad() {
+                        SR.init({
+                            trackerKey: "47f16347-5035-4d85-ab34-7a340428ebac",
+                            plugin: "faststore"
+                        }).then(function () {
+                            SR.event.pageVisit().then(function () {
+                                SR.dynamicContent.get();
+                            });
+                        });
+                    }
+
+                    (function(s,y,n,e,r,i,se){s["SyneriseObjectNamespace"]=r;s[r]=s[r]||[],
+                        s[r]._t=1*new Date(),s[r]._i=0,s[r]._l=i;var z=y.createElement(n),
+                        se=y.getElementsByTagName(n)[0];z.async=1;z.src=e;se.parentNode.insertBefore(z,se);
+                        z.onload=z.onreadystatechange=function(){var rdy=z.readyState;
+                            if(!rdy||/complete|loaded/.test(z.readyState)){s[i]();z.onload = null;
+                                z.onreadystatechange=null;}};})(window,document,"script", "//web.snrbox.com/synerise-javascript-sdk.min.js", "SR", "onSyneriseLoad");
                     `,
-                }}
-            />
-        </>
+            }}
+        />
     )
 }
 
-export default ThirdPartyScripts;
+export default ThirdPartyScripts
