26ef8e0796515a64588db1db30db1147a843c041
kamilkanik <kamil.kanik@icloud.com>
2025-04-04 15:05:00 +0200

    Added transaction source into orderFormCustomData

--- src/components/sections/GlobalTracker/GlobalTrackerSection.tsx
+++ src/components/sections/GlobalTracker/GlobalTrackerSection.tsx
@@ -1,10 +1,9 @@
 import {useEffect} from "react";
 import Cookies from 'js-cookie';
+import isMobile from "is-mobile";
 
 import {request} from "src/sdk/graphql/request";
 import {gql} from "@generated/gql";
-
-
 
 const UpdateOrderFormCustomDataMutation = gql(`
     mutation addOrderFormCustomData($appId: String!, $field: String!, $value: String!){
@@ -24,9 +23,11 @@ const GlobalTrackerSection = () => {
     useEffect(() => {
         const uuid = Cookies.get('_snrs_uuid');
         const snrsParams = Cookies.get('_snrs_params');
+        const source = isMobile() ? "WEB_MOBILE" : "WEB_DESKTOP";
 
         if(uuid) updateSyneriseCustomApp('uuid', uuid);
         if(snrsParams) updateSyneriseCustomApp('snrs_params', snrsParams);
+        if(source) updateSyneriseCustomApp('source', source);
     }, []);
 
     return <></>
