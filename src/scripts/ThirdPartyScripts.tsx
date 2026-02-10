24bba3c5b7e4a5d3c10614c972b2f2d1431e82ee
Bartłomiej Kowalczuk <bartek62345@gmail.com>
2026-02-10 13:08:28 +0100

    feat: update dependencies and new tracker

--- src/scripts/ThirdPartyScripts.tsx
+++ src/scripts/ThirdPartyScripts.tsx
@@ -1,30 +1,29 @@
-
 const ThirdPartyScripts = () => {
-    return (
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `
-                    function onSyneriseLoad() {
-                        SR.init({
-                            trackerKey: "47f16347-5035-4d85-ab34-7a340428ebac",
-                            plugin: "faststore"
-                        }).then(function () {
-                            SR.event.pageVisit().then(function () {
-                                SR.dynamicContent.get();
-                            });
-                        });
-                    }
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            function onSyneriseLoad() {
+                SR.init({
+                  trackerKey: "b8349350-8569-4232-99a8-0e08818c3b99",
+                  plugin: "faststore"
+                }).then(function () {
+                    SR.event.pageVisit().then(function () {
+                        SR.dynamicContent.get();
+                    });
+                });
+            }
+                    
+            (function(s,y,n,e,r,i,se){s["SyneriseObjectNamespace"]=r;s[r]=s[r]||[],
+            s[r]._t=1*new Date(),s[r]._i=0,s[r]._l=i;var z=y.createElement(n),
+            se=y.getElementsByTagName(n)[0];z.async=1;z.src=e;se.parentNode.insertBefore(z,se);
+            z.onload=z.onreadystatechange=function(){var rdy=z.readyState;
+            if(!rdy||/complete|loaded/.test(z.readyState)){s[i]();z.onload = null;
+            z.onreadystatechange=null;}};})(window,document,"script", "//web.azu.snrbox.com/synerise-javascript-sdk.min.js", "SR", "onSyneriseLoad");
+        `,
+      }}
+    />
+  );
+};
 
-                    (function(s,y,n,e,r,i,se){s["SyneriseObjectNamespace"]=r;s[r]=s[r]||[],
-                        s[r]._t=1*new Date(),s[r]._i=0,s[r]._l=i;var z=y.createElement(n),
-                        se=y.getElementsByTagName(n)[0];z.async=1;z.src=e;se.parentNode.insertBefore(z,se);
-                        z.onload=z.onreadystatechange=function(){var rdy=z.readyState;
-                            if(!rdy||/complete|loaded/.test(z.readyState)){s[i]();z.onload = null;
-                                z.onreadystatechange=null;}};})(window,document,"script", "//web.snrbox.com/synerise-javascript-sdk.min.js", "SR", "onSyneriseLoad");
-                    `,
-            }}
-        />
-    )
-}
-
-export default ThirdPartyScripts
+export default ThirdPartyScripts;
